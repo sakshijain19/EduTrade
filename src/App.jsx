29ba8c43bb13ac6f14384005bfb5d7e6fb4fc990
SanchetiRaksha <rakshasancheti03@gmail.com>
2025-02-13 19:43:39 +0530

    Chatbot Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
 
 import './App.css'
+import Chatbot from "./components/Chatbot"; // Adjust the path if needed
+
 
 
 function App() {
 
 
   return (
-    <>
-
-    </>
+    <div>
+      <h1>Welcome to EduTrade</h1>
+      <Chatbot />
+    </div>
   )
 }
 
 
 export default App
+
+
+
+
